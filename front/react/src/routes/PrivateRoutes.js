b1940356cd9e143dbe4a5cac8a59238bfef66cf5
EmilianoMunoz <e.munoz@um.edu.ar>
2023-10-17 18:11:49 -0300

    Avances en entregable

--- front/react/src/routes/PrivateRoutes.js
+++ front/react/src/routes/PrivateRoutes.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Route, Routes, Navigate} from "react-router-dom";
-
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Offers } from '../components/Offers';
 import { UserList } from '../components/UserList';
-import { ParkingForm } from '../components/ParkingForm'
+import { ParkingForm } from '../components/ParkingForm';
+import Home from '../components/Home'; // Importa tu componente Home aquí
 
 export const PrivateRoutes = () => {
   return (
     <Routes>
+        <Route path='/home' element={<Home />} /> {/* Ruta para el componente Home */}
         <Route path='/offers' element={<Offers />} />
         <Route path='/userList' element={<UserList />} />
         <Route path='/parkingform' element={<ParkingForm />} />
